9070eeb6c82e9ab60a0349fcb1563713fa114dd9
tejas <tejasshah@gmail.com>
2023-05-07 13:05:02 +0530

    private url 1

---
+++ src/main/java/com/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/category")
+@Controller
+public class CategoryController {
+
+	// http://localhost:9999/category
+	@GetMapping("/new")
+	public String newCategory(HttpSession session, Model model) {
+
+		// if login ? then go ahead
+		// else login
+		if (session.getAttribute("user") == null) {
+			model.addAttribute("error", "Please Login Before Accessing the service...");
+			return "Login";
+		} else {
+			return "NewCategory";
+		}
+	}
+
+	// http://localhost:9999/category/all
+	@GetMapping("/all")
+	public String getAllCategory(Model model,HttpSession session) {
+		// if login ? then go ahead
+		// else login
+		if (session.getAttribute("user") == null) {
+			model.addAttribute("error", "Please Login Before Accessing the service...");
+			return "Login";
+		} else {
+			return "ListCategory";
+		}
+
+	}
+}
